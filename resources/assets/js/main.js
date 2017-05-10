723e94003e32da7cc4f261a0eb2b347bf6b248ed
Victor Nwaiwu <victor.nwaiwu@andela.com>
2017-05-10 12:16:14 +0100

    refactor: refactor code in gisview component

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,30 +1,29 @@
-import GISView from './components/GISView.vue';
-// const GISView = require('./components/GISView.vue');
+const gisview = require('./components/GISView.vue');
+
 window.Vue = Vue;
 window.Event = new class {
-    constructor() {
-        this.Vue = new Vue();
-    }
+  constructor() {
+    this.Vue = new Vue();
+  }
 
-    fire(event, data = null) {
-        this.Vue.$emit(event, data);
-    }
+  fire(event, data = null) {
+    this.Vue.$emit(event, data);
+  }
 
-    listen(event, callback) {
-        this.Vue.$on(event, callback);
-    }
+  listen(event, callback) {
+    this.Vue.$on(event, callback);
+  }
 };
 
 window.app = new Vue({
-    el: '#app',
-    components: {
-        'gisview': {GISView}
-    },
-    data: {
-    },
-    methods: {
-        init: function() {
-            this.$broadcast('MapsApiLoaded');
-        }
+  el: '#app',
+  components: {
+    gisview
+  },
+  data: {},
+  methods: {
+    init: function() {
+      this.$broadcast('MapsApiLoaded');
     }
+  }
 });
