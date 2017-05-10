daf2a1103b584b6a3e2039783e3a5e04e87b2909
Victor Nwaiwu <victor.nwaiwu@andela.com>
2017-05-10 12:14:37 +0100

    chore(webpack): edit webpack config to compile main.js seperately

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,16 +15,17 @@ mix.sass('resources/assets/sass/app.scss', 'public/css');
 mix.copy('resources/assets/vendor/bootstrap/fonts', 'public/fonts');
 mix.copy('resources/assets/vendor/font-awesome/fonts', 'public/fonts')
 mix.styles([
-    'resources/assets/vendor/bootstrap/css/bootstrap.css',
-    'resources/assets/vendor/animate/animate.css',
-    'resources/assets/vendor/font-awesome/css/font-awesome.css',
+  'resources/assets/vendor/bootstrap/css/bootstrap.css',
+  'resources/assets/vendor/animate/animate.css',
+  'resources/assets/vendor/font-awesome/css/font-awesome.css',
 ], 'public/css/vendor.css', './');
 mix.scripts([
-    'resources/assets/vendor/jquery/jquery-3.1.1.min.js',
-    'resources/assets/vendor/bootstrap/js/bootstrap.js',
-    'resources/assets/vendor/metisMenu/jquery.metisMenu.js',
-    'resources/assets/vendor/slimscroll/jquery.slimscroll.min.js',
-    'resources/assets/vendor/pace/pace.min.js',
-    'resources/assets/js/app.js',
-    'resources/assets/js/main.js',
+  'resources/assets/vendor/jquery/jquery-3.1.1.min.js',
+  'resources/assets/vendor/bootstrap/js/bootstrap.js',
+  'resources/assets/vendor/metisMenu/jquery.metisMenu.js',
+  'resources/assets/vendor/slimscroll/jquery.slimscroll.min.js',
+  'resources/assets/vendor/pace/pace.min.js',
+  'resources/assets/js/app.js'
 ], 'public/js/app.js', './');
+
+mix.js(['resources/assets/js/main.js'], 'public/js/main.js');
